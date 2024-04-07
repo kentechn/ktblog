6a83dfcc3a13d490d8e8d4ee2058b81692f9fa1d
kentechn <cdspyk9236@gmail.com>
2024-04-08 00:12:53 +0900

    update2

--- components/toc/Toc.vue
+++ components/toc/Toc.vue
@@ -40,12 +40,12 @@ tocs.forEach((toc) => {
 <template>
   <div class="p-4 bg-base-200 rounded-md">
     <BaseText size="lg" weight="bold"> 目次 </BaseText>
-    <ul class="menu bg-base-200 rounded-box mt-3 inline-block w-full break-all text-wrap">
-      <li v-for="item in parentTocList" :key="item.id" class="py-1 text-wrap inline-block w-full break-all">
-        <a :href="`#${item.id}`" class="inline-block">{{ item.text }}</a>
-        <ul v-if="item.children.length > 0" class="block break-all text-wrap">
-          <li v-for="childToc in item.children" :key="childToc.id" class="py-1 text-wrap break-all">
-            <a :href="`#${item.id}`" class="text-wrap  inline-block w-full bg-primary" style="text-wrap: wrap !important; overflow-wrap: break-all !important">{{ childToc.text }}</a>
+    <ul class="menu bg-base-200 rounded-box mt-3 inline-block w-full break-all text-wrap !break-words">
+      <li v-for="item in parentTocList" :key="item.id" class="py-1 text-wrap inline-block w-full break-all !break-words">
+        <a :href="`#${item.id}`" class="inline-block !break-words">{{ item.text }}</a>
+        <ul v-if="item.children.length > 0" class="block break-all text-wrap !break-words">
+          <li v-for="childToc in item.children" :key="childToc.id" class="py-1 text-wrap break-all !break-words">
+            <a :href="`#${item.id}`" class="text-wrap  inline-block w-full !break-words" style="text-wrap: wrap !important; overflow-wrap: break-all !important">{{ childToc.text }}</a>
           </li>
         </ul>
       </li>
