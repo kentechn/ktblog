9c742e329b7384619b34823c5afeda43d2473f1e
kentechn <cdspyk9236@gmail.com>
2024-04-08 00:59:34 +0900

    update9

--- components/toc/Toc.vue
+++ components/toc/Toc.vue
@@ -41,53 +41,23 @@ tocs.forEach((toc) => {
   <div class="p-4 bg-base-200 rounded-md">
     <BaseText size="lg" weight="bold"> 目次 </BaseText>
     <ul
-      class="menu bg-base-200 rounded-box mt-3 inline-block w-full break-all text-wrap !break-words"
+      class="menu bg-base-200 rounded-box mt-3 inline-block w-full break-all text-wrap"
     >
       <li
         v-for="item in parentTocList"
         :key="item.id"
-        class="py-1 text-wrap inline-block w-full break-all !break-words"
-        style="
-          overflow-wrap: anywhere !important;
-          word-break: break-all !important;
-        "
+        class="py-1 text-wrap inline-block w-full break-all"
+        style="overflow-wrap: break-all"
       >
-        <a
-          :href="`#${item.id}`"
-          class="block !break-words"
-          style="
-            overflow-wrap: anywhere !important;
-            word-break: break-all !important;
-          "
-          >{{ item.text }}</a
-        >
-        <ul
-          v-if="item.children.length > 0"
-          class="block break-all text-wrap !break-words"
-          style="
-            overflow-wrap: anywhere !important;
-            word-break: break-all !important;
-          "
-        >
+        <a :href="`#${item.id}`" class="block">{{ item.text }}</a>
+        <ul v-if="item.children.length > 0" class="block break-all text-wrap">
           <li
             v-for="childToc in item.children"
             :key="childToc.id"
-            class="py-1 text-wrap break-all !block !w-full"
-            style="
-              overflow-wrap: anywhere !important;
-              word-break: break-all !important;
-            "
+            class="py-1 text-wrap break-all block w-full"
+            style="overflow-wrap: break--all"
           >
-            <a
-              :href="`#${item.id}`"
-              class=""
-              style="
-                overflow-wrap: anywhere !important;
-                word-break: break-all !important;
-              "
-            >
-              {{ childToc.text }}</a
-            >
+            <a :href="`#${item.id}`">{{ childToc.text }}</a>
           </li>
         </ul>
       </li>
