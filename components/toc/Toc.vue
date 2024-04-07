9cf9f652698742663055477aeddef8f9b1345e2c
kentechn <cdspyk9236@gmail.com>
2024-04-07 23:53:11 +0900

    目次スタイル更新

--- components/toc/Toc.vue
+++ components/toc/Toc.vue
@@ -40,10 +40,10 @@ tocs.forEach((toc) => {
 <template>
   <div class="p-4 bg-base-200 rounded-md">
     <BaseText size="lg" weight="bold"> 目次 </BaseText>
-    <ul class="menu bg-base-200 rounded-box mt-3">
-      <li v-for="item in parentTocList" :key="item.id" class="py-1 text-wrap">
-        <a :href="`#${item.id}`">{{ item.text }}</a>
-        <ul v-if="item.children.length > 0">
+    <ul class="menu bg-base-200 rounded-box mt-3 block w-full">
+      <li v-for="item in parentTocList" :key="item.id" class="py-1 text-wrap block w-full">
+        <a :href="`#${item.id}`" class="block">{{ item.text }}</a>
+        <ul v-if="item.children.length > 0" class="block">
           <li v-for="childToc in item.children" :key="childToc.id" class="py-1 text-wrap block">
             <a :href="`#${item.id}`" class="text-wrap block w-full bg-primary" style="text-wrap: wrap !important; overflow-wrap: break-all !important">{{ childToc.text }}</a>
           </li>
